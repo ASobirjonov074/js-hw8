ed7bbd34f335b72e6af89c3c787a16fba5563968
ASobirjonov074 <jonic607@gmail.com>
2023-06-30 16:19:16 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+// let num = prompt("Write a number.")
+
+// if(num % 2 === 0) {
+//     alert('odd')
+// } else {
+//     alert('even')
+// }
+
+
+
+// let name = ['Daler', 'Ruxshod', 'Amir', 'Doni', 'Abdullah']
+// let num = prompt('Write a number from 1 to 5')
+
+// if(num >= 1) {
+//     name.splice(num - 1, 1);
+//     console.log('New Massive', name);
+// } else {
+//     console.log('You have written incorrect password');
+// }
+
+
+// let names = ['Daler', 'Ruxshod', 'Amir', 'Doni', 'Abdullah']
+// let longName = names.reduce(function (longest, currentName) {
+//     if (currentName.length > longest.length) {
+//         return currentName;
+//     }
+//     return longest;
+// },);
+// console.log(names);
+
